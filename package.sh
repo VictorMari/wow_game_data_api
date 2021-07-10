af60d282826a6177852e16529b9b4fb6a8bc6429
Victor Mari Rodriguez <vrodriguez@tmforum.org>
2021-07-10 17:39:15 +0100

    adding package script

---
+++ package.sh
@@ -0,0 +1,5 @@
+python -m pip install --upgrade pip
+python -m pip install --upgrade build
+python -m pip install --upgrade twine
+python -m build
+python -m twine upload --repository testpypi dist/*
